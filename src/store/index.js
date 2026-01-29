8f73e1bf591ed280350d054b037597813a66dc99
sky_song <120517787+Haneul-Song@users.noreply.github.com>
2026-01-29 15:01:57 -0700

    Implement features section

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import { texture } from 'three/tsl';
 import { create } from 'zustand';
 
 const useMacBookStore = create((set) => ({
@@ -7,7 +8,10 @@ const useMacBookStore = create((set) => ({
     scale: 0.08,
     setScale: (scale) => set({ scale }),
 
-    reset: () => set({ color: '#2e2c2e', scale: 0.08 }),
+    texture: '/videos/feature-1.mp4',
+    setTexture: (texture) => set({ texture }),
+
+    reset: () => set({ color: '#2e2c2e', scale: 0.08, texture: '/videos/features-1.mp4' }),
 }))
 
 export default useMacBookStore
